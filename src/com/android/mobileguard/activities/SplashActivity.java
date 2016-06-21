c67ed4607bc3392964489b0e50c793abda099909
feng <linfeng_myl@sina.cn>
2016-06-21 09:23:37 +0800

    报名命名同意规范为com.andorid.mobileguard.

---
+++ src/com/android/mobileguard/activities/SplashActivity.java
@@ -0,0 +1,40 @@
+package com.android.mobileguard.activities;
+
+import com.android.mobileguard.utils.PackageInfoUtils;
+import com.androidpro.mobilesafety.R;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class SplashActivity extends Activity {
+	
+	
+	
+	private TextView tv_splash_version;
+	
+	
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        tv_splash_version = (TextView) findViewById(R.id.tv_splash_version);
+        
+        String version = PackageInfoUtils.getPackageVersion(this);
+        tv_splash_version.setText("版本号："+version+"\n©2016黑马版权�?�?");
+        
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.splash, menu);
+        return true;
+    }
+    
+}
